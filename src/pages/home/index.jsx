69f46c2a452b93a9850486d0ec889e1a027d0f96
Felipe da Silva <silvaafelipe016@gmail.com>
2024-06-11 13:12:00 -0300

    feat: updates in the home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,8 @@
-import "./styles.css";
+import styles from "./Home.module.css";
 
 function Home() {
   return (
-    <section className="section-container">
+    <section className={styles.section_container}>
       <div>
         <h1>Seu café perfeito, onde e quando quiser.</h1>
       </div>
