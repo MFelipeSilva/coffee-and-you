7db17df6832c03f8686c2cbac133a972893518d1
Felipe da Silva <silvaafelipe016@gmail.com>
2024-06-13 13:11:02 -0300

    feat: added carousel in the categories component

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,20 +1,68 @@
+import Slider from "react-slick";
 import { categories } from "../../utils/categories";
 
 import styles from "./Categories.module.css";
 
 export const Categories = () => {
+  const settings = {
+    dots: false,
+    infinite: false,
+    arrows: false,
+    initialSlides: false,
+    speed: 400,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2.5,
+          slidesToScroll: 2.5,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1.5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1.1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <section className={styles.section_container}>
       <div className={styles.content}>
         <h2 className={styles.subtitle}>Principais categorias</h2>
-        <div className={styles.cards_content}>
+        <Slider {...settings} className={styles.slick_container}>
           {categories.map((category) => (
             <div key={category.id} className={styles.category_card}>
               <h3>{category.title}</h3>
               <img src={category.image} alt="categories of coffees" />
             </div>
           ))}
-        </div>
+        </Slider>
       </div>
     </section>
   );
