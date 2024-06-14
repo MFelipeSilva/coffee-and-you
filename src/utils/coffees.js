a66c2d6b1594edbefcece9d59f8c53c2d8b14346
Felipe da Silva <silvaafelipe016@gmail.com>
2024-06-14 20:52:24 -0300

    feat(assets): changed name of images

--- src/utils/coffees.js
+++ src/utils/coffees.js
@@ -1,44 +1,44 @@
-import coffe_image_1 from "../assets/coffe-image-1.png";
-import coffe_image_2 from "../assets/coffe-image-2.png";
-import coffe_image_3 from "../assets/coffe-image-3.png";
-import coffe_image_4 from "../assets/coffe-image-4.png";
-import coffe_image_5 from "../assets/coffe-image-5.png";
-import coffe_image_6 from "../assets/coffe-image-6.png";
+import coffee_image_1 from "../assets/coffee-image-1.png";
+import coffee_image_2 from "../assets/coffee-image-2.png";
+import coffee_image_3 from "../assets/coffee-image-3.png";
+import coffee_image_4 from "../assets/coffee-image-4.png";
+import coffee_image_5 from "../assets/coffee-image-5.png";
+import coffee_image_6 from "../assets/coffee-image-6.png";
 
 export const coffees = [
   {
     id: "1",
     title: "Expresso Tradicional",
     description: "O tradicional café feito com água quente e grãos moídos",
-    image: coffe_image_1,
+    image: coffee_image_1,
     price: 5.99,
   },
   {
     id: "2",
     title: "Expresso Americano",
     description: "Expresso diluído, menos intenso que o tradicional",
-    image: coffe_image_2,
+    image: coffee_image_2,
     price: 7.99,
   },
   {
     id: "3",
     title: "Expresso Cremoso",
     description: "Café  cremoso tradicional com espuma cremosa",
-    image: coffe_image_3,
+    image: coffee_image_3,
     price: 8.99,
   },
   {
     id: "4",
     title: "Café com Leite",
     description: "Meio a meio de expresso tradicional com leite vaporizado",
-    image: coffe_image_4,
+    image: coffee_image_4,
     price: 6.55,
   },
   {
     id: "65",
     title: "Chocolate Quente",
     description: "Bebida feita com chocolate dissolvido no leite quente e café",
-    image: coffe_image_5,
+    image: coffee_image_5,
     price: 7.59,
   },
   {
@@ -46,7 +46,7 @@ export const coffees = [
     title: "Café Latte",
     description:
       "Uma dose de café expresso com o dobro de leite e espuma cremosa",
-    image: coffe_image_6,
+    image: coffee_image_6,
     price: 9.99,
   },
 ];
